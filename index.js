50a5ad8ad777e624aee5b525978fdf09b8233ade
Jeleven-11 <yukikitazawa03@gmail.com>
2025-01-14 22:15:16 +0800

    new file:   index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// app/page.js
+'use client';
+import { useEffect, useState } from 'react';
+import HomeClient from '/components/HomeClient';
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+      <HomeClient />
+    </div>
+  );
+};
+
+export default Home;
